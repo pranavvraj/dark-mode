065dd0575792f0310751a176a0008dd2d92563a5
pranavvraj <pranavraj3638@gmail.com>
2021-10-08 02:05:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+        <span className="lightClass" style={{ color: darkMode ? "grey" : "orange" }}>☀︎</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span className="darkClass" style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+      </div>
+       <div className="container">
+        <h1 style={{ color: darkMode ? "white" : "orange" }}>Good {darkMode ? "Night." : "Morning!"} </h1>
+      </div>
     </div>
   );
 }
-
 export default App;
