38e068cb7f8b2dcb92e396ffdaf94cf1db893469
pranavvraj <pranavraj3638@gmail.com>
2021-10-08 17:42:06 +0530

    design changes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
   return (
     <div className={darkMode ? "dark-mode" : "light-mode"}>
       <div className="container">
-        <span className="lightClass" style={{ color: darkMode ? "grey" : "orange" }}>☀︎</span>
-        <div className="switch-checkbox">
-          <label className="switch">
-            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
-            <span className="slider round"> </span>
-          </label>
-        </div>
-        <span className="darkClass" style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
-      </div>
-       <div className="container">
         <h1 style={{ color: darkMode ? "white" : "orange" }}>Good {darkMode ? "Night." : "Morning!"} </h1>
-      </div>
+        <div className="icon-part">
+          <span className="lighticon" style={{ color: darkMode ? "grey" : "orange" }}>☀︎</span>
+          <div className="switch-checkbox">
+            <label className="switch">
+              <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+              <span className="slider round"> </span>
+            </label>
+          </div>
+          <span className="darkicon" style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+        </div>
     </div>
+  </div>
   );
 }
 export default App;
